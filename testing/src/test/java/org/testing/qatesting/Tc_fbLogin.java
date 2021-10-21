bf5fa6cc7889fd56119afc0042c540fabf297868
ramugandham <ramugandham1223@gmail.com>
2021-10-21 16:53:53 +0530

    second project

---
+++ testing/src/test/java/org/testing/qatesting/Tc_fbLogin.java
@@ -0,0 +1,20 @@
+package org.testing.qatesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Tc_fbLogin {
+	@Test
+	public void login() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		 WebDriver driver=new ChromeDriver();
+         driver.manage().deleteAllCookies();
+         driver.manage().window().maximize();
+         driver.get("https://facebook.com");
+         driver.quit();
+	}
+
+}
