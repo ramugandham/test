d2bfa7fda50e3e420404cbb6c8d0df3884bdfdfa
ramugandham <92849687+ramugandham@users.noreply.github.com>
2021-10-28 13:50:50 +0530

    Update Factorial.java
    factorial programme

--- testing/src/main/java/org/testing/devcode/Factorial.java
+++ testing/src/main/java/org/testing/devcode/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	  
+	         // factorial programe on 5*4*3*2=120
 		      int  fact=1;
 		      int num=5;
 		      for(int i=1;i<=num;i++) {
